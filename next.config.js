b4528311170b6dbf2110ba5a8e9c5fcf087338ad
Carson <rodriguescarson@hotmail.com>
2023-02-05 22:45:38 +0530

    First working version! 🎉

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replicate.com",
+      },
+      {
+        protocol: "https",
+        hostname: "replicate.delivery",
+      },
+    ],
+  },
 }
