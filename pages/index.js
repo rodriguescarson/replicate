94531be229758e0c3d8ac21936c816b3e9f120a0
Carson <rodriguescarson@hotmail.com>
2023-02-06 00:14:43 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -8,6 +8,20 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 export default function Home() {
   const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
+  const [allPredictions, setAllPredictions] = useState(null);
+  useEffect(() => {
+    getAllPredictions();
+  }, []);
+  const getAllPredictions = async () => {
+    const response = await fetch("/api/predictions/getAllPredictions");
+    let allPredictions = await response.json();
+    if (response.status !== 200) {
+      setError(allPredictions.detail);
+      return;
+    }
+    setAllPredictions(allPredictions);
+    console.log({ allPredictions })
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -53,10 +67,15 @@ export default function Home() {
           Diffusion AI
         </h1>
         <p>
-          Designed by <a href="https://stability.ai">Stability AI</a>, Stable Diffusion is a GPT-3 model that generates images from text prompts. This demo uses the <a href="https://replicate.com">Replicate</a> API to run the model.
+          Designed by <a className={styles.a} href="https://stability.ai">Stability AI</a>, Stable Diffusion is a GPT-3 model that generates images from text prompts. This demo uses the <a href="https://replicate.com" className={styles.a}>Replicate</a> API to run the model.
         </p>
-        <br />
-
+        <p style={
+          {
+          fontStyle: 'italic',
+          }
+        }>
+          Website by Carson Rodrigues
+        </p>
       </div>
       <div className={styles.body}>
         <form className={styles.form} onSubmit={handleSubmit}>
@@ -82,9 +101,39 @@ export default function Home() {
           </div>
         )}
       </div>
+      <div className={styles.carousel}>
+      <h2>
+        Predictions from the past prompts:
+        </h2>
+        <div style={{
+          margin: 'auto',
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignContent: 'center',
+          width: '100%',
+        }}> 
+        {allPredictions && allPredictions.map((prediction, i) => (
+          <Image
+            key={i}
+            width={200}
+            height={200}
+            src={prediction.output[prediction.output.length - 1]}
+            alt={`output ${i}`}
+            style={{
+              margin: '10px',
+              borderRadius: '10px',
+              boxShadow: '0 0 10px 0 rgba(0,0,0,0.5)'
+
+            }}
+            />
+        ))}
+          </div>
+      </div>
       <div className={styles.footer}>
 
-        Visit Carsons linkedin  <a href="https://www.linkedin.com/in/rodriguescarson/">@rodriguescarson</a>
+        Visit Carsons linkedin  <a className={styles.a} href="https://www.linkedin.com/in/rodriguescarson/">@rodriguescarson</a>
 
       </div>
     </div>
